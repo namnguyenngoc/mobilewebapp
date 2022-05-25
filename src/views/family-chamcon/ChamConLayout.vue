35c4878023a4bba6d7fd4ad1b6779ea3840deaed
namnguyenngoc <itnguyennam@gmail.com>
2022-05-25 16:57:57 +0700

    update css

--- src/views/family-chamcon/ChamConLayout.vue
+++ src/views/family-chamcon/ChamConLayout.vue
@@ -32,6 +32,7 @@
               :responsive="true"
               :margin="[10, 10]"
               :use-css-transforms="true"
+              @breakpoint-changed="breakpointChangedEvent"
             >
                 <grid-item v-for="item in layout" :key="item.i"
                     :x="item.x"
@@ -307,6 +308,9 @@ export default {
     openDetail(ngay){
       console.log('ngày', ngay);
       this.$refs.chamConListDialog.dialog = true;
+    },
+    breakpointChangedEvent: function(newBreakpoint, newLayout){
+        console.log("BREAKPOINT CHANGED breakpoint=", newBreakpoint, ", layout: ", newLayout );
     }
     
   },
@@ -358,5 +362,56 @@ export default {
     min-height:100px;
     max-height: 700px;
     overflow: auto; 
+  }
+
+
+  .vue-grid-layout {
+    background: #312d4b;
+  }
+  .vue-grid-item:not(.vue-grid-placeholder) {
+      background:#312d4b;
+      border: 1px solid black;
+  }
+  .vue-grid-item .resizing {
+      opacity: 0.9;
+  }
+  .vue-grid-item .static {
+      background: #cce;
+  }
+  .vue-grid-item .text {
+      font-size: 24px;
+      text-align: center;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      height: 100%;
+      width: 100%;
+  }
+  .vue-grid-item .no-drag {
+      height: 100%;
+      width: 100%;
+  }
+  .vue-grid-item .minMax {
+      font-size: 12px;
+  }
+  .vue-grid-item .add {
+      cursor: pointer;
+  }
+  .vue-draggable-handle {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      top: 0;
+      left: 0;
+      background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>") no-repeat;
+      background-position: bottom right;
+      padding: 0 8px 8px 0;
+      background-repeat: no-repeat;
+      background-origin: content-box;
+      box-sizing: border-box;
+      cursor: pointer;
   }
 </style>
